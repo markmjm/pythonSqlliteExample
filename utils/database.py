d49042a78ce9fc6c309e3c6cde453cb506ea2cd7
markmjm <markmnei@gmail.com>
2018-03-20 20:17:13 -0400

    initial commit

--- utils/database.py
+++ utils/database.py
@@ -1,23 +1,26 @@
+from typing import List, Dict, Union
 from .database_connection import DatabaseConnection
 
 """
 Concerned with storing and retrieving books from database
 """
 
+Book = Dict(str, Union(str, int))
 
-def create_book_table():
+
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS books (name text, author text, read integer)")
 
 
-def add_book(name, author):
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute(f'insert into books values(?, ? , 0)', (name, author))
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute("select * from books")
@@ -25,13 +28,13 @@ def get_all_books():
     return books
 
 
-def update_read_book(name):
+def update_read_book(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('update books set read=1 where name=?', (name,))
 
 
-def delete_book(name):
+def delete_book(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('delete from books where name = ?', (name,))
